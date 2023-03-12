e34b8b4add13616e21b4c1d5c9199b7b4698c02c
Asha Luckins <ashaluckinsanoop@gmail.com>
2023-03-11 21:22:56 -0800

    remove the expiration from the local storage during logout

--- frontend/src/pages/Logout.js
+++ frontend/src/pages/Logout.js
@@ -3,5 +3,6 @@ import { redirect } from "react-router-dom";
 export function action() {
   console.log("Inside logout action");
   localStorage.removeItem("token");
+  localStorage.removeItem("expiration");
   return redirect("/");
 }
